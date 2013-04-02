af53029618bcc777b0bdedb7246387b6d74bc487
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2013-04-02 11:57:05 +0400

    Task 7_2 fixed

--- tasks7/task7_2/uniqueList.h
+++ tasks7/task7_2/uniqueList.h
@@ -6,25 +6,16 @@
 template <typename Type> class UniqueList : public SingleLinkedList<Type>
 {
 public:
-    void addElement(Type val) throw(AddExistedExc*)
+    void addElement(Type val) throw(AddExistedExc*);
+};
+
+template <typename Type>
+void UniqueList<Type>::addElement(Type val) throw(AddExistedExc*)
+{
     {
         if (this->exist(val))
             throw new AddExistedExc;
-        ListElement<Type>* newElem = new ListElement<Type>(val);
-        if (this->currentElem == this->headElem)
-        {
-            newElem->setNext(this->headElem);
-            this->headElem = newElem;
-            this->currentElem = this->headElem;
-        }
-        else
-        {
-            newElem->setNext(this->currentElem);
-            this->prev();
-            this->currentElem->setNext(newElem);
-            this->next();
-        }
-        this->elemCount++;
+        SingleLinkedList<Type>::addElement(val);
     }
-};
+}
 
